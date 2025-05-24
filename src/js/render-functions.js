709ad4cd39a84206297415272060562ad83d116e
kearlock <kearlock@gmail.com>
2025-05-24 22:28:15 +0300

    font style fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,35 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// ========================================================================
-// const exampleImage = {
-//     "id": 1527602,
-//     "pageURL": "https://pixabay.com/photos/lobster-food-ide-1527602/",
-//     "type": "photo",
-//     "tags": "lobster, food, ide, lobster, lobster, lobster, lobster, lobster",
-//     "previewURL": "https://cdn.pixabay.com/photo/2016/07/19/06/14/lobsters-1527602_150.jpg",
-//     "previewWidth": 150,
-//     "previewHeight": 99,
-//     "webformatURL": "https://pixabay.com/get/gfb82d1505cfa5ff6ad3cb3ba55f1e615b1e0e5915db2e8724f153bbcc487b08e0a0bbde857550f128f964502ead168e4bd5b94bdf4a3d8fab5e632f3adafd666_640.jpg",
-//     "webformatWidth": 640,
-//     "webformatHeight": 426,
-//     "largeImageURL": "https://pixabay.com/get/gd393a423c0066821d6ebb42409e0f8ba6e33d808e97c5affae66073e3c68969004f9565cb876743a0b76b45cef0fce6f124e098af78a7960c039b9cf7e383bd5_1280.jpg",
-//     "imageWidth": 3072,
-//     "imageHeight": 2048,
-//     "imageSize": 2596565,
-//     "views": 11657,
-//     "downloads": 6152,
-//     "collections": 280,
-//     "likes": 28,
-//     "comments": 4,
-//     "user_id": 2419035,
-//     "user": "ksbaeg",
-//     "userImageURL": "https://cdn.pixabay.com/user/2022/03/26/07-11-47-551_250x250.jpg",
-//     "noAiTraining": true
-// }
-// ========================================================================
-
-// find gallery element
 const gallery = document.querySelector('.gallery');
 
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -39,7 +10,6 @@ const lightbox = new SimpleLightbox('.gallery a', {
 });
 
 export function createGallery(images) {
-  // Create gallery
   const markup = images
     .map(
       ({
@@ -82,7 +52,7 @@ export function createGallery(images) {
   gallery.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
 }
-// function clearGallery
+
 export function clearGallery() {
   gallery.innerHTML = '';
 }
